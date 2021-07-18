2127a0cad100c5cffd6fcdb385bcde20264b9795
getraid <32432718+getraid@users.noreply.github.com>
2021-07-18 13:43:59 +0200

    Added pwa support (dirty)

--- JapaneseLearningLogFrontend/vue.config.js
+++ JapaneseLearningLogFrontend/vue.config.js
@@ -4,5 +4,19 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  outputDir: "../static"
+  outputDir: "../static",
+  pwa: {
+    name: "Japanese Log",
+    themeColor: "#DF7323",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      favicon32: "/public/favicon.png",
+      favicon16: "/public/favicon.png",
+      appleTouchIcon: "/public//favicon.png",
+      maskIcon: "/public/favicon.png",
+      msTileImage: "/public/favicon.png"
+    }
+  }
 };
